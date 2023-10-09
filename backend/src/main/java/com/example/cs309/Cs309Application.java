25f9ff913802d13afeb62df78ff6d160a845028a
Cooper <12110517@mail.sustech.edu.cn>
2023-10-09 21:07:43 +0800

    remove "com.example.cs309.domain", add cooper's database settings

--- backend/src/main/java/com/example/cs309/Cs309Application.java
+++ backend/src/main/java/com/example/cs309/Cs309Application.java
@@ -3,11 +3,11 @@ package com.example.cs309;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.cs309.domain"})
-public class  Cs309Application {
+@SpringBootApplication()
+public class Cs309Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cs309Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Cs309Application.class, args);
+    }
 
 }
