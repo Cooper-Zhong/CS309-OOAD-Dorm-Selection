2358a16f392f3c1c600cbcea22752bc724e3d535
Cooper <12110517@mail.sustech.edu.cn>
2023-10-10 09:14:44 +0800

    xq辛苦了

--- backend/src/main/java/cs309_dorm_backend/CS309Application.java
+++ backend/src/main/java/cs309_dorm_backend/CS309Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-public class cs309Application {
+public class CS309Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(cs309Application.class, args);
+        SpringApplication.run(CS309Application.class, args);
     }
 
 }
