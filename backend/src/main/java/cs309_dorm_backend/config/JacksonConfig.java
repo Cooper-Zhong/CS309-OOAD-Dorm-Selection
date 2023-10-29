ee04b916af5033c62c233703334eedbffcdb0c73
Cooper <12110517@mail.sustech.edu.cn>
2023-10-30 00:37:08 +0800

    allow serializing null

---
+++ backend/src/main/java/cs309_dorm_backend/config/JacksonConfig.java
@@ -0,0 +1,17 @@
+package cs309_dorm_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // 序列化空对象时不会引发异常。
+        return objectMapper;
+    }
+}
