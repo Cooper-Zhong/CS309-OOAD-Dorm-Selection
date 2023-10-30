5a7dd497f6ea6735187bb827cd41b348e07e0914
Cooper <12110517@mail.sustech.edu.cn>
2023-10-30 21:26:03 +0800

    serialize building as its buildingId only.

--- backend/src/main/java/cs309_dorm_backend/domain/Room.java
+++ backend/src/main/java/cs309_dorm_backend/domain/Room.java
@@ -3,6 +3,7 @@ package cs309_dorm_backend.domain;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Setter
 @Getter
-@Table(name = "rooms")
+@Table(name = "rooms", uniqueConstraints = @UniqueConstraint(columnNames = {"building_id", "room_number"}))
 public class Room {
 
     @Id
@@ -20,6 +21,10 @@ public class Room {
     private int roomId;
 
     @NotNull
+    @JsonAlias("buildingId")
+    @JsonIdentityReference(alwaysAsId = true) //当序列化 Room 实体时，只会包含 Building 的 buildingId 属性
+    //使用 buildingId 属性作为标识来识别 Building 实体。
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "buildingId")
     @ManyToOne(fetch = FetchType.LAZY) // a room belongs to a building
     @JoinColumn(name = "building_id", referencedColumnName = "building_id")
     private Building building;
@@ -40,17 +45,20 @@ public class Room {
 
     private String description;
 
+
+    @JsonIgnore
     @OneToMany(mappedBy = "room", fetch = FetchType.LAZY) // a room can have many comments, or no comments
     private List<Comment> comments;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "favoriteRooms", fetch = FetchType.LAZY) // a room can be favorited by many teams
     private List<Team> favoriteTeams;
 
+
+    @JsonIgnore
     @OneToOne(fetch = FetchType.LAZY)
     // a room can be assigned to one team
     @JoinColumn(name = "team_id", referencedColumnName = "team_id")
     private Team assignedTeam; // a room can only be assigned to one team
-
-
 }
 
