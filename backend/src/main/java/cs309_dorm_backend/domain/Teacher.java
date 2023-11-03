7176a1294f907fcecdc4c9bff03b8b7355809ec8
Cooper <12110517@mail.sustech.edu.cn>
2023-11-03 11:52:41 +0800

    teacher operations

--- backend/src/main/java/cs309_dorm_backend/domain/Teacher.java
+++ backend/src/main/java/cs309_dorm_backend/domain/Teacher.java
@@ -19,7 +19,7 @@ public class Teacher {
     @JsonIgnore
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId  // 使用 @MapsId 注解，以便将主键的值映射到外键列
-    @JoinColumn(name = "teacher_id", referencedColumnName = "campus_id")
+    @JoinColumn(name = "teacher_id", referencedColumnName = "campus_id", nullable = false)
     private User user;
 
     @Column(name = "name")
