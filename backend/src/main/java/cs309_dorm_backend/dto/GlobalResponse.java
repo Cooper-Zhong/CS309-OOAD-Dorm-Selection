158cab4ef77566d6b28818ce31307b94d308793f
Cooper <12110517@mail.sustech.edu.cn>
2023-11-03 11:52:04 +0800

    global response and exception handling

---
+++ backend/src/main/java/cs309_dorm_backend/dto/GlobalResponse.java
@@ -0,0 +1,16 @@
+package cs309_dorm_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GlobalResponse<T> {
+    int code;
+    String msg;
+    T data; //controller return data
+}
