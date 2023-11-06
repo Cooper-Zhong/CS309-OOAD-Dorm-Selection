85e8f17368f6dc4250008dbe6ca2d4ff129738b6
Cooper <12110517@mail.sustech.edu.cn>
2023-11-06 13:11:59 +0800

    hello world test

--- backend/src/main/java/cs309_dorm_backend/web/hello.java
+++ backend/src/main/java/cs309_dorm_backend/web/hello.java
@@ -2,10 +2,25 @@ package cs309_dorm_backend.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.HashMap;
+import java.util.Map;
+//
+//@RestController
+//public class hello {
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        return "Hello world";
+//    }
+//}
+
 public class hello {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hello world";
+
+    private int a = 1;
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        int val = map.get("b");
+        System.out.println(val);
     }
 }
+
