17da5f270238b30411449b677451f195b9f651dd
Cooper <12110517@mail.sustech.edu.cn>
2023-11-29 00:11:40 +0800

    sql triggers

---
+++ backend/src/main/java/cs309_dorm_backend/sql_files/console.sql
@@ -0,0 +1,17 @@
+-- student
+insert into users (campus_id, password, role)
+values ('111', '111', 'student');
+insert into users (campus_id, password, role)
+values ('112', '112', 'student');
+insert into users (campus_id, password, role)
+values ('113', '113', 'student');
+
+insert into students (student_id, degree, gender, info, major, name)
+values ('111', 'undergraduate', 'male', 'info', 'computer science', 'name');
+
+-- teacher
+insert into users (campus_id, password, role)
+values ('30001000', '30001000', 'teacher');
+insert into users (campus_id, password, role)
+values ('30001001', '30001001', 'teacher');
+
