d41026525da60c0d494b81d78c4beaff98a17aff
Cooper <12110517@mail.sustech.edu.cn>
2023-11-29 00:10:01 +0800

    support student delete

--- backend/src/main/java/cs309_dorm_backend/dao/StudentRepo.java
+++ backend/src/main/java/cs309_dorm_backend/dao/StudentRepo.java
@@ -3,6 +3,8 @@ package cs309_dorm_backend.dao;
 import cs309_dorm_backend.domain.Student;
 import cs309_dorm_backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,10 @@ import java.util.List;
 @Repository
 @Component
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+
+    @Modifying
+    @Query(value = "update students set team_id = null where student_id = :studentId", nativeQuery = true)
+    void removeTeam(int studentId);
 
 }
