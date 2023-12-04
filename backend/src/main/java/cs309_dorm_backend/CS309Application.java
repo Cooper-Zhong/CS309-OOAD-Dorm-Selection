4ced8565d3f605bd8ec70879b57dd3bcecef69e0
Cooper <12110517@mail.sustech.edu.cn>
2023-12-04 20:39:23 +0800

    update

--- backend/src/main/java/cs309_dorm_backend/CS309Application.java
+++ backend/src/main/java/cs309_dorm_backend/CS309Application.java
@@ -2,10 +2,12 @@ package cs309_dorm_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication()
+@EnableCaching
 @EnableSwagger2
 @CrossOrigin(origins = "*") // 允许所有域的跨域请求
 public class CS309Application {
