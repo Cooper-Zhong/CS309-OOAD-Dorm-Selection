988c973851dba3e81a630a40ecea4d4207dfe3d3
Skylar <12110943@mail.sustech.edu.cn>
2024-01-01 17:16:33 +0800

    反爬虫写好了 现在反爬虫定的是anti.reptile.manager.include-urls=^/team/.*$,^/dorm/.*$ team和dorm的信息 只改动了property文件
    密码存储写好了 存储的应该是加密后的内容 只改动了user相关的文件
    
    单一登陆是通过一个id 一个session内存储存实现的 也改动的是user相关的文件
    
    redisson文件是想实现redis的功能 但是redis本身的内存占用和对于数据CURD效率的提高表现不明显 redis文件夹下的内容可以不用 redis的持久化的需求也比较大 总之就是不适合redis

--- backend/src/main/java/cs309_dorm_backend/service/user/UserService.java
+++ backend/src/main/java/cs309_dorm_backend/service/user/UserService.java
@@ -6,15 +6,19 @@ import cs309_dorm_backend.dto.UserForm;
 import cs309_dorm_backend.dto.UserUpdateDto;
 import org.springframework.validation.BindingResult;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 public interface UserService {
 
     UserDto updatePassword(UserUpdateDto userUpdateDto, BindingResult result);
     User findByCampusId(String campusId);
 
-    Boolean checkLogin(UserDto userInfo);
+    Boolean checkLogin(UserDto userInfo, HttpSession session, Map<String, String> userSessions);
+//    Boolean checkLogin(UserDto userInfo);
+
 
     UserDto register(@Valid UserForm userForm, BindingResult result);
 
